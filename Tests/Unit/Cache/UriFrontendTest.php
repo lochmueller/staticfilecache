d8064d9b5045183b96bdeda8b7eaf552600e55de
Tim Lochmüller <tim@fruit-lab.de>
2016-09-13 22:17:33 +0200

    Add first test case

---
+++ Tests/Unit/Cache/UriFrontendTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Test the URI frontend
+ *
+ * @author  Tim Lochmüller
+ */
+
+namespace SFC\Staticfilecache\Tests\Unit\Cache;
+
+use SFC\Staticfilecache\Cache\UriFrontend;
+use TYPO3\CMS\Core\Cache\Backend\NullBackend;
+
+/**
+ * Test the URI frontend
+ */
+class UriFrontendTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @test
+     */
+    public function checkValidPath()
+    {
+        $this->assertSame(true, $this->getCacheFrontend()
+            ->isValidEntryIdentifier('http://www.domain.tld/path.html'));
+    }
+
+    /**
+     * @test
+     */
+    public function checkInValidPath()
+    {
+        $this->assertSame(false, $this->getCacheFrontend()
+            ->isValidEntryIdentifier('/path.html'));
+    }
+
+    /**
+     * Get the subject
+     *
+     * @return UriFrontend
+     */
+    protected function getCacheFrontend()
+    {
+        return new UriFrontend('test', new NullBackend(''));
+    }
+
+}
