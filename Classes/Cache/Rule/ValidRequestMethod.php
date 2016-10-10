4861bd86ba5408c2659db5ca198358bad4852e24
Tim Lochmüller <tim.lochmueller@hdnet.de>
2016-10-10 13:36:02 +0200

    Add Rule ValidRequestMethod

--- Classes/Cache/Rule/ValidRequestMethod.php
+++ Classes/Cache/Rule/ValidRequestMethod.php
@@ -1,10 +1,32 @@
 <?php
 
+/**
+ * ValidRequestMethod
+ */
 
 namespace SFC\Staticfilecache\Cache\Rule;
 
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
-class ValidRequestMethod
+/**
+ * ValidRequestMethod
+ */
+class ValidRequestMethod extends AbstractRule
 {
 
+    /**
+     * Method to check the rul and modify $explanation and/or $skipProcessing
+     *
+     * @param TypoScriptFrontendController $frontendController
+     * @param string $uri
+     * @param array $explanation
+     * @param bool $skipProcessing
+     */
+    protected function checkRule($frontendController, $uri, &$explanation, &$skipProcessing)
+    {
+        if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+            $explanation[__CLASS__] = 'The request methode has to be GET';
+            $skipProcessing = true;
+        }
+    }
 }
