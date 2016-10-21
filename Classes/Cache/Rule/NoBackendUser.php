d955005b1e963982e0fcd88160378009a7dd016d
Tim Lochmüller <tim.lochmueller@hdnet.de>
2016-10-21 16:13:51 +0200

    [BUGFIX] Check also the 'cookie only' in the BE user rule

--- Classes/Cache/Rule/NoBackendUser.php
+++ Classes/Cache/Rule/NoBackendUser.php
@@ -15,7 +15,7 @@ use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 class NoBackendUser extends AbstractRule
 {
 
-    /**
+   /**
      * No active BE user
      *
      * @param TypoScriptFrontendController $frontendController
@@ -25,9 +25,32 @@ class NoBackendUser extends AbstractRule
      */
     public function checkRule($frontendController, $uri, &$explanation, &$skipProcessing)
     {
-        if ($frontendController->isBackendUserLoggedIn()) {
+        if ($frontendController->isBackendUserLoggedIn() || $this->hasActiveBackendCookies()) {
             $skipProcessing = true;
-            $explanation[__CLASS__] = 'Active BE Login (TSFE:beUserLogin)';
+            $explanation[__CLASS__] = 'Active BE Login or active BE login session (TSFE:beUserLogin)';
         }
+    }
+
+    /**
+     * Has active backend cookies
+     *
+     * The Server rewrite rules can only check the existence of an cookie and not the valid
+     * auth process behind. This is the reason, why we call a user with a BE cookie "active"
+     * even if the users cookie is actually a invalid/old one.
+     *
+     * @return bool
+     */
+    protected function hasActiveBackendCookies()
+    {
+        $cookieNames = [
+            'be_typo_user',
+            $GLOBALS['TYPO3_CONF_VARS']['BE']['cookieName']
+        ];
+        foreach ($cookieNames as $cookieName) {
+            if (isset($_COOKIE[$cookieName])) {
+                return true;
+            }
+        }
+        return false;
     }
 }
