83a5465127af4fb992724d6032a6f88c7e741478
Tim Lochmüller <tim@fruit-lab.de>
2017-02-16 15:57:59 +0100

    Allow TYPO3 8.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Static File Cache Team',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-7.99.99',
+            'typo3' => '6.2.0-8.6.99',
             'php' => '5.6.0-0.0.0',
         ],
     ],
