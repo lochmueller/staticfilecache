c84d6d0d8c679c2fd2cf37585da74ddf16035822
Tim Lochmüller <tim.lochmueller@hdnet.de>
2017-08-04 17:04:16 +0200

    Refactoring the BE module

--- Classes/Module/CacheModule.php
+++ Classes/Module/CacheModule.php
@@ -12,8 +12,8 @@ namespace SFC\Staticfilecache\Module;
 
 use SFC\Staticfilecache\Service\CacheService;
 use TYPO3\CMS\Backend\Module\AbstractFunctionModule;
-use TYPO3\CMS\Backend\Tree\View\BrowseTreeView;
 use TYPO3\CMS\Backend\Utility\BackendUtility;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\View\StandaloneView;
 
@@ -38,66 +38,52 @@ class CacheModule extends AbstractFunctionModule
     public function main()
     {
         $this->handleActions();
-
         $this->pageId = intval($this->pObj->id);
-
-        /* @var $tree BrowseTreeView */
-        $tree = GeneralUtility::makeInstance(BrowseTreeView::class);
-        $tree->makeHTML = 2;
-        $tree->init();
-
-        // Set starting page Id of tree (overrides webmounts):
-        if ($this->pageId > 0) {
-            $tree->MOUNTS = [0 => $this->pageId];
-        }
-
-        $tree->ext_IconMode = true;
-        $tree->showDefaultTitleAttribute = true;
-        $tree->thisScript = BackendUtility::getModuleUrl(GeneralUtility::_GP('M'));
-        $tree->treeName = 'staticfilecache';
-
-        $tree->getBrowsableTree();
-
-        return $this->processExpandCollapseLinks($this->renderModule($tree));
+        return $this->renderModule();
     }
 
     /**
      * Rendering the information
      *
-     * @param    BrowseTreeView $tree The Page tree data
-     *
      * @return    string        HTML for the information table.
      */
-    protected function renderModule(BrowseTreeView $tree)
+    protected function renderModule()
     {
         $rows = [];
         $cache = GeneralUtility::makeInstance(CacheService::class)->getCache();
 
-        foreach ($tree->tree as $row) {
-            $cacheEntries = $cache->getByTag('sfc_pageId_' . $row['row']['uid']);
+
+        /** @var ConnectionPool $connectionPool */
+        $connectionPool = GeneralUtility::makeInstance(ConnectionPool::class);
+        $queryBuilder = $connectionPool->getQueryBuilderForTable('pages');
+        $dbRows = $queryBuilder->select('*')
+            ->from('pages')
+            ->where($queryBuilder->expr()->eq('pid', $queryBuilder->createNamedParameter($this->pageId)))
+            ->execute()
+            ->fetchAll();
+
+        foreach ($dbRows as $row) {
+            $cacheEntries = $cache->getByTag('sfc_pageId_' . $row['uid']);
+
             if ($cacheEntries) {
-                $isFirst = true;
                 foreach ($cacheEntries as $identifier => $info) {
                     $cell = [
-                        'uid' => $row['row']['uid'],
-                        'title' => $isFirst ? $row['HTML'] . BackendUtility::getRecordTitle(
+                        'uid' => $row['uid'],
+                        'title' => BackendUtility::getRecordTitle(
                             'pages',
-                            $row['row'],
+                            $row,
                             true
-                        ) : $row['HTML_depthData'],
+                        ),
                         'identifier' => $identifier,
                         'info' => $info,
-                        'depthData' => $row['depthData'],
                     ];
-                    $isFirst = false;
 
                     $rows[] = $cell;
                 }
             } else {
                 $cell = [
-                    'uid' => $row['row']['uid'],
-                    'title' => $row['HTML'] . BackendUtility::getRecordTitle('pages', $row['row'], true),
-                    'depthData' => $row['depthData'],
+                    'uid' => $row['uid'],
+                    'title' => BackendUtility::getRecordTitle('pages', $row, true),
                 ];
                 $rows[] = $cell;
             }
@@ -128,27 +114,5 @@ class CacheModule extends AbstractFunctionModule
         if (isset($action['removeExpiredPages']) && (bool)$action['removeExpiredPages']) {
             GeneralUtility::makeInstance(CacheService::class)->getCache()->collectGarbage();
         }
-    }
-
-    /**
-     * Processes the expand/collapse links and adds the Id of the current page in branch.
-     *
-     * Example:
-     * index.php?PM=0_0_23_staticfilecache#0_23 --> index.php?PM=0_0_23_staticfilecache&id=13#0_23
-     *
-     * @param    string $content : Content to be processed
-     *
-     * @return    string        The processed and modified content
-     */
-    protected function processExpandCollapseLinks($content)
-    {
-        if (strpos($content, 'PM=') !== false && $this->pageId > 0) {
-            $content = preg_replace(
-                '/(href=")([^"]+PM=[^"#]+)(#[^"]+)?(")/',
-                '${1}${2}&id=' . $this->pageId . '${3}${4}',
-                $content
-            );
-        }
-        return $content;
     }
 }
