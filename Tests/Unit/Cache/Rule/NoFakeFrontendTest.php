c5a6890ec4747e102e6fe00075d58360603d7cc1
Tim Lochmüller <tim.lochmueller@hdnet.de>
2018-07-12 09:31:33 +0200

    Update DEV dependencies and fix one Unit Test

--- Tests/Unit/Cache/Rule/NoFakeFrontendTest.php
+++ Tests/Unit/Cache/Rule/NoFakeFrontendTest.php
@@ -1,10 +1,15 @@
 <?php
 
-declare(strict_types = 1);
 /**
  * Test the Fake Frontend Rule.
  */
+
+declare(strict_types = 1);
+
 namespace SFC\Staticfilecache\Tests\Unit\Cache\Rule;
+
+use TYPO3\CMS\Core\Cache\CacheManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Test the Fake Frontend Rule.
@@ -14,8 +19,17 @@ class NoFakeFrontendTest extends \PHPUnit\Framework\TestCase
     /**
      * @test
      */
-    public function checkValidPath()
+    public function checkNoFakeFrontendController()
     {
+        // Init the cache_pages cache, to avoid exceptions in thes TSFE building process
+        $cacheManager = GeneralUtility::makeInstance(CacheManager::class);
+        $cacheManager->setCacheConfigurations([
+            'cache_pages' => [
+                'frontend' => \TYPO3\CMS\Core\Cache\Frontend\VariableFrontend::class,
+                'backend' => \TYPO3\CMS\Core\Cache\Backend\NullBackend::class,
+            ],
+        ]);
+
         $tsfe = new \TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController([], 0, 0);
         $uri = '';
         $explanation = [];
