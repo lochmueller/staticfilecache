79f0e0eaa3f5ad89b2481245b1a3aedbbdcc153c
Tim Lochmüller <tim@fruit-lab.de>
2018-09-09 12:29:19 +0200

    Set compatibility up to 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Static File Cache Team',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-9.3.99',
+            'typo3' => '8.7.0-9.5.99',
             'php' => '7.0.0-0.0.0',
         ],
     ],
