7b5061e50d0716b220ce889ff48f2beb4215f5fa
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-04-03 12:07:06 +0200

    Fix #140 - Check if Brotli is supported before calling brotli_compress

--- Classes/Generator/AbstractGenerator.php
+++ Classes/Generator/AbstractGenerator.php
@@ -7,13 +7,27 @@ declare(strict_types = 1);
 
 namespace SFC\Staticfilecache\Generator;
 
+use Psr\Log\LoggerAwareTrait;
 use SFC\Staticfilecache\StaticFileCacheSingletonInterface;
+use TYPO3\CMS\Core\Log\LogManager;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * AbstractGenerator.
  */
 abstract class AbstractGenerator implements StaticFileCacheSingletonInterface
 {
+
+    use LoggerAwareTrait;
+
+    /**
+     * AbstractGenerator constructor.
+     */
+    public function __construct()
+    {
+        $this->setLogger(GeneralUtility::makeInstance(LogManager::class)->getLogger(__CLASS__));
+    }
+
     /**
      * Generate file.
      *
--- Classes/Generator/BrotliGenerator.php
+++ Classes/Generator/BrotliGenerator.php
@@ -24,6 +24,9 @@ class BrotliGenerator extends AbstractGenerator
      */
     public function generate(string $entryIdentifier, string $fileName, string $data)
     {
+        if (!$this->checkAvailable()) {
+            return;
+        }
         $contentCompress = \brotli_compress($data);
         if ($contentCompress) {
             GeneralUtility::writeFile($fileName . '.br', $contentCompress);
@@ -38,7 +41,24 @@ class BrotliGenerator extends AbstractGenerator
      */
     public function remove(string $entryIdentifier, string $fileName)
     {
+        if (!$this->checkAvailable()) {
+            return;
+        }
         $removeService = GeneralUtility::makeInstance(RemoveService::class);
         $removeService->removeFile($fileName . '.br');
+    }
+
+    /**
+     * Check if Brotli is available
+     *
+     * @return bool
+     */
+    protected function checkAvailable(): bool
+    {
+        $available = function_exists('brotli_compress');
+        if (!$available) {
+            $this->logger->error('Your server do not support Botli compression, but you enable Brotli in EXT:staticfilecache configuration');
+        }
+        return $available;
     }
 }
