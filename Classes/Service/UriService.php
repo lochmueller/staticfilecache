7e0e15ec97052b78d58dbee4782123945764bb7c
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-09-26 16:25:36 +0200

    Drop the recrete URI. The times of this are over

--- Classes/Service/UriService.php
+++ Classes/Service/UriService.php
@@ -32,10 +32,6 @@ class UriService extends AbstractService
         // Find host-name / IP, always in lowercase:
         $isHttp = (0 === \mb_strpos(GeneralUtility::getIndpEnv('TYPO3_REQUEST_HOST'), 'http://'));
         $uri = GeneralUtility::getIndpEnv('REQUEST_URI');
-        if ($configuration->isBool('recreateURI')) {
-            $uri = $this->recreateUriPath($uri);
-        }
-
         $uri = ($isHttp ? 'http://' : 'https://') . \mb_strtolower(GeneralUtility::getIndpEnv('HTTP_HOST')) . '/' . \ltrim($uri, '/');
 
         try {
@@ -49,40 +45,5 @@ class UriService extends AbstractService
             // The URI is already in puny code (no logging needed)
         }
         return $uri;
-    }
-
-    /**
-     * Recreates the URI of the current request.
-     *
-     * Especially in simulateStaticDocument context, the different URIs lead to the same result
-     * and static file caching would store the wrong URI that was used in the first request to
-     * the website (e.g. "TheGoodURI.13.0.html" is as well accepted as "TheFakeURI.13.0.html")
-     *
-     * @param string $uri
-     *
-     * @return string The recreated URI of the current request
-     */
-    protected function recreateUriPath($uri): string
-    {
-        $objectManager = GeneralUtility::makeInstance(ObjectManager::class);
-        /** @var UriBuilder $uriBuilder */
-        $uriBuilder = $objectManager->get(UriBuilder::class);
-        if (null === ObjectAccess::getProperty($uriBuilder, 'contentObject', true)) {
-            // there are situations without a valid contentObject in the URI builder
-            // prevent this situation by return the original request URI
-            return $uri;
-        }
-        $url = $uriBuilder->reset()
-            ->setAddQueryString(true)
-            ->setCreateAbsoluteUri(true)
-            ->build();
-
-        $parts = (array)\parse_url($url);
-        $unset = ['scheme', 'user', 'pass', 'host', 'port'];
-        foreach ($unset as $u) {
-            unset($parts[$u]);
-        }
-
-        return HttpUtility::buildUrl($parts);
     }
 }
