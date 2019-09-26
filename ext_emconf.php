c03368b6fbf6f2d3a9277ee52b7115f6cf5f6db1
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-09-26 15:07:36 +0200

    Drop TYPO3 8.x support

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,7 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'StaticFileCache',
     'description' => 'Transparent StaticFileCache solution using mod_rewrite and mod_expires. Increase performance for static pages by a factor of 230!!',
     'category' => 'fe',
-    'version' => '9.0.0',
+    'version' => '10.0.0-dev',
     'state' => 'stable',
     'modify_tables' => 'pages',
     'clearcacheonload' => true,
@@ -15,8 +15,8 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'StaticFileCache Team',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.0.99',
-            'php' => '7.0.0-0.0.0',
+            'typo3' => '9.5.0-10.0.99',
+            'php' => '7.2.0-0.0.0',
         ],
     ],
     'autoload' => [
