18460f473046d591a3ff7121c5ef3c60c5ef7738
Tim Lochmüller <tim.lochmueller@hdnet.de>
2019-10-04 14:47:02 +0200

    Set version for TYPO3 10 up to 10.9

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'StaticFileCache Team',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.1.99',
+            'typo3' => '9.5.0-10.9.99',
             'php' => '7.2.0-0.0.0',
         ],
     ],
