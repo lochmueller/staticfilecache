5d027eae87839384dcb1ffa8afb63cbf1427971d
Tim Lochmüller <tim@fruit-lab.de>
2020-02-11 22:05:38 +0100

    Fix #232 - Add more details to the disableStaticFileCache configuration of the site configuration

--- Configuration/SiteConfiguration/Overrides/sites.php
+++ Configuration/SiteConfiguration/Overrides/sites.php
@@ -2,6 +2,7 @@
 
 $GLOBALS['SiteConfiguration']['site']['columns']['disableStaticFileCache'] = [
     'label' => 'Disable StaticFileCache',
+    'description' => 'Note: If the static file cache of a site was enabled (default!) and you disable the cache, please take care to drop the static file cache manually or run `typo3cms staticfilecache:flushCache --force-boost-mode-flush` once!',
     'config' => [
         'type' => 'check',
     ],
