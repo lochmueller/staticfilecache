936fc32db8300b025f292bec518dc4a1580bc1e8
Konrad Michalik <konrad.michalik@xima.de>
2020-03-02 11:04:26 +0100

    [Task] Support PHP 7.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
-            'php' => '7.3.0-7.4.99',
+            'php' => '7.2.0-7.4.99',
         ],
     ],
     'autoload' => [
