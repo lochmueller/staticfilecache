8b9f6fe1be7838a07694958bed49c05d9602c767
Tim Lochmüller <tim@fruit-lab.de>
2020-04-21 19:58:59 +0200

    Clenaups

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,20 +5,19 @@ declare(strict_types=1);
 $EM_CONF[$_EXTKEY] = [
     'title' => 'StaticFileCache',
     'description' => 'Transparent StaticFileCache solution using mod_rewrite and mod_expires. Increase performance for static pages by a factor of 230!!',
-    'category' => 'fe',
     'version' => '11.0.0',
-    'state' => 'stable',
-    'modify_tables' => 'pages',
-    'clearcacheonload' => true,
-    'author' => 'StaticFileCache Team',
-    'author_email' => 'tim@fruit-lab.de',
-    'author_company' => 'StaticFileCache Team',
+    'category' => 'fe',
     'constraints' => [
         'depends' => [
             'typo3' => '9.5.0-10.4.99',
             'php' => '7.2.0-7.4.99',
         ],
     ],
+    'state' => 'stable',
+    'clearCacheOnLoad' => true,
+    'author' => 'StaticFileCache Team',
+    'author_email' => 'tim@fruit-lab.de',
+    'author_company' => 'StaticFileCache Team',
     'autoload' => [
         'psr-4' => [
             'SFC\\Staticfilecache\\' => 'Classes'
