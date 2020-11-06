2a7b8dcc3b03190c84bf873c1edc550d66b60ccc
Tim Lochmüller <tim@fruit-lab.de>
2020-11-06 10:09:46 +0100

    Fix #274 - remove usage of ManifestService

--- Classes/Generator/ManifestGenerator.php
+++ Classes/Generator/ManifestGenerator.php
@@ -8,12 +8,15 @@ declare(strict_types=1);
 namespace SFC\Staticfilecache\Generator;
 
 use Psr\Http\Message\ResponseInterface;
-use SFC\Staticfilecache\Service\ManifestService;
-use SFC\Staticfilecache\Service\RemoveService;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+// use SFC\Staticfilecache\Service\ManifestService;
+// use SFC\Staticfilecache\Service\RemoveService;
+// use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * ManifestGenerator.
+ *
+ * @todo implement
  */
 class ManifestGenerator extends AbstractGenerator
 {
@@ -22,12 +25,12 @@ class ManifestGenerator extends AbstractGenerator
      */
     public function generate(string $entryIdentifier, string $fileName, ResponseInterface $response, int $lifetime): void
     {
-        $manifestService = GeneralUtility::makeInstance(ManifestService::class);
-        $html = (string) $response->getBody();
-        $content = $manifestService->generateManifestContent($entryIdentifier, $html);
-        if ('' !== $content) {
-            GeneralUtility::writeFile($fileName.'.sfc', $content);
-        }
+        // $manifestService = GeneralUtility::makeInstance(ManifestService::class);
+        // $html = (string) $response->getBody();
+        // $content = $manifestService->generateManifestContent($entryIdentifier, $html);
+        // if ('' !== $content) {
+        // GeneralUtility::writeFile($fileName.'.sfc', $content);
+        // }
     }
 
     /**
@@ -35,7 +38,7 @@ class ManifestGenerator extends AbstractGenerator
      */
     public function remove(string $entryIdentifier, string $fileName): void
     {
-        $removeService = GeneralUtility::makeInstance(RemoveService::class);
-        $removeService->file($fileName.'.sfc');
+        // $removeService = GeneralUtility::makeInstance(RemoveService::class);
+        // $removeService->file($fileName.'.sfc');
     }
 }
