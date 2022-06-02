ca252ce5ba809a190379ab6f78f443c87a7e39f9
Marcus Förster <m.foerster@xerc.de>
2022-06-02 14:47:11 +0200

    [DEA] risyk one-line JS

--- Classes/Service/InlineAssets/InlineScripts.php
+++ Classes/Service/InlineAssets/InlineScripts.php
@@ -33,14 +33,14 @@ class InlineScripts extends AbstractInlineAssets
 
             if ($this->configurationService->get('inlineScriptMinify')) {
                 $fileSrc = preg_replace('/^\s*\/\/.*$/m', '', $fileSrc); // remove single-line comments
-                if (!preg_match('/(?<![\'":])\/\//', $fileSrc)) { // RISKY; https?://|"//|'//
-                    $fileSrc = preg_replace('/\v+/', '', $fileSrc); // remove line-breaks
-                }
+                // if (!preg_match('/(?<![\'":])\/\//', $fileSrc)) { // RISKY; https?://|"//|'//
+                //     $fileSrc = preg_replace('/\v+/', '', $fileSrc); // remove line-breaks
+                // }
                 $fileSrc = preg_replace('/\h+/', ' ', $fileSrc); // shrink whitespace
 
                 $fileSrc = preg_replace('/\/\*.*?\*\//s', '', $fileSrc); // remove multi-line comments
                 $fileSrc = preg_replace('/ *([({,;:<>=*+\-\/&?})]) */', '$1', $fileSrc); // remove no-req. spaces
-                $fileSrc = preg_replace('/;(?=})|(?<=});/', '', $fileSrc); // shorten function endings
+                $fileSrc = preg_replace('/;(?=})/', '', $fileSrc); // shorten function endings
             }
 
             $content = str_replace($matches[0][$index], '<script>'.rtrim($fileSrc), $content);
