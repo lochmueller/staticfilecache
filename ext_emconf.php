7f59125859a2488eea18ee601050732c10783ac3
Tim <tim@fruit-lab.de>
2022-12-16 11:00:03 +0100

    Prepare next major release

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,8 +9,8 @@ $EM_CONF[$_EXTKEY] = [
     'category' => 'fe',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.21-11.5.99',
-            'php' => '7.4.0-8.99.99',
+            'typo3' => '11.5.0-12.5.99',
+            'php' => '8.0.0-8.99.99',
         ],
     ],
     'state' => 'stable',
