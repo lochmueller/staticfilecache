bae5c9de6b870eeea649e13b529bf7d6de3fc89f
Tim Lochmüller <tim.lochmueller@hdnet.de>
2023-09-29 13:05:22 +0200

    chore: Change ext_emconf.php version to 13.1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'StaticFileCache',
     'description' => 'Transparent static file cache solution using mod_rewrite and mod_expires. Increase performance for static pages by a factor of 230!!',
-    'version' => '13.1.2',
+    'version' => '13.1.3',
     'category' => 'fe',
     'constraints' => [
         'depends' => [
