3179154ee554e2d04bdaeb0bd62b0221f1370442
Bastien Lutz <bastien.lutz@in2code.de>
2026-05-28 15:23:32 +0200

    [BUGFIX] Don't cache pages during solr indexing
    In EXT:solr version 14, indexing requests are
    executed via subrequests instead of HTTP/HTTPS.
    
    Additionally, query parameters were moved
    to a custom request attribute
    `solr.indexingInstructions`,
    which still allows identifying indexing requests.
    
    Due to the change to subrequests, it seems
    necessary to also trigger the
    `SolrIndexingProcessListener` with the
    CacheRuleEvent (PrepareMiddleware).
    
    Resolves: #464

--- Classes/Cache/Listener/SolrIndexingProcessListener.php
+++ Classes/Cache/Listener/SolrIndexingProcessListener.php
@@ -4,16 +4,16 @@ declare(strict_types=1);
 
 namespace SFC\Staticfilecache\Cache\Listener;
 
-use SFC\Staticfilecache\Event\CacheRuleFallbackEvent;
+use SFC\Staticfilecache\Event\CacheRuleEventInterface;
 
 /**
  * Solr Indexing process.
  */
 class SolrIndexingProcessListener
 {
-    public function __invoke(CacheRuleFallbackEvent $event): void
+    public function __invoke(CacheRuleEventInterface $event): void
     {
-        if ($event->getRequest()->hasHeader('X-Tx-Solr-Iq')) {
+        if ($event->getRequest()->getAttribute('solr.indexingInstructions', null) !== null) {
             $event->addExplanation(__CLASS__, 'Solr Indexing request');
             $event->setSkipProcessing(true);
         }
