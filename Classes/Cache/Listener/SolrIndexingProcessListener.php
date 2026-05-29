fc8abd3c5ca7c8a1b7a6010c40b241817db22968
Tim Lochmüller <tim.lochmueller@hdnet.de>
2026-05-29 08:52:40 +0200

    fix: handling of new EXT:solr requests

--- Classes/Cache/Listener/SolrIndexingProcessListener.php
+++ Classes/Cache/Listener/SolrIndexingProcessListener.php
@@ -13,6 +13,10 @@ class SolrIndexingProcessListener
 {
     public function __invoke(CacheRuleFallbackEvent $event): void
     {
+        // Note!! This is handled by old EXT:solr versions!!!
+        // The new version set the 'frontend.cache.instruction' in the
+        // right way and is cached via CachingAllowedListener.
+
         if ($event->getRequest()->hasHeader('X-Tx-Solr-Iq')) {
             $event->addExplanation(__CLASS__, 'Solr Indexing request');
             $event->setSkipProcessing(true);
